703828ce5397f1db63bcc232f9a36da0f9e83fba
Tyler Ross <tross020890@gmail.com>
2022-08-10 00:23:51 -0500

    Test: sessions rejects wrong passwords, unregistered email

--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -17,4 +17,34 @@ RSpec.describe "Sessions Request" do
     expect(response_body[:data][:attributes][:email]).to eq("tim@example.com")
     expect(response_body[:data][:attributes][:api_key]).to be_a(String)
   end
+
+  it 'rejects unregistered email' do
+    User.create(email: "tim@example.com", password: "password", api_key: "buncharandomchars")
+    header = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+    body = {
+            "email": "timmmy@badexample.com",
+            "password": "password"
+            }
+
+    post '/api/v1/sessions', headers: header, params: JSON.generate(body)
+    response_body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.status).to eq(404)
+    expect(response_body[:error]).to eq("Invalid Credentials")
+  end
+
+  it 'rejects wrong password' do
+    User.create(email: "tim@example.com", password: "password", api_key: "buncharandomchars")
+    header = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+    body = {
+            "email": "timmmy@example.com",
+            "password": "badpassword"
+            }
+
+    post '/api/v1/sessions', headers: header, params: JSON.generate(body)
+    response_body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.status).to eq(404)
+    expect(response_body[:error]).to eq("Invalid Credentials")
+  end
 end
