f3bce6efc0898cbcffc97be09f37e8b889cce0d6
Tyler Ross <tross020890@gmail.com>
2022-08-10 07:13:51 -0500

    Feat: roadtrip endpoint returns serialized roadtrip

--- spec/requests/api/v1/roadtrips_request_spec.rb
+++ spec/requests/api/v1/roadtrips_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Roadtrips Request" do
-  it 'authenticates and returns user' do
+  it 'returns roadtrip json', :vcr do
     User.create(email: "tim@example.com", password: "password", api_key: "buncharandomchars")
     header = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
     body = {
@@ -12,11 +12,12 @@ RSpec.describe "Roadtrips Request" do
 
     post '/api/v1/road_trip', headers: header, params: JSON.generate(body)
     response_body = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
 
     expect(response.status).to eq(200)
-    expect(response_body[:data][:id]).to be_an(Integer)
-    expect(response_body[:data][:attributes][:email]).to eq("tim@example.com")
-    expect(response_body[:data][:attributes][:api_key]).to be_a(String)
+    expect(response_body[:data][:attributes][:start_city]).to eq("Denver,CO")
+    expect(response_body[:data][:attributes][:end_city]).to eq("Pueblo,CO")
+    expect(response_body[:data][:attributes][:travel_time]).to be_a(String)
+    expect(response_body[:data][:attributes][:weather_at_eta][:temperature]).to be_an(Integer)
+    expect(response_body[:data][:attributes][:weather_at_eta][:conditions]).to be_a(String)
   end
 end
